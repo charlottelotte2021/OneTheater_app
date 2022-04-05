293fcfb984ea6431b4516bb9221370d9f46611ee
charlottelotte2021 <charlotteguen@gmail.com>
2022-04-05 10:10:33 +0200

    routes for the single play pages

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,25 @@ const express = require('express');
 const router  = express.Router();
 const {ensureAuthenticated} = require("../config/auth.js")
 const Play = require("../models/play").Play
-const playInstance = require("../models/playInstance").playInstance
+const PlayInstance = require("../models/playInstance").PlayInstance
 const Theater = require("../models/theater").Theater
-
+// const playId = req.params.ObjectId
+// const playsInstancesId = req.params.ObjectId
 // const dotenv = require(“dotenv”);
 // dotenv.config();
 
 const getAllPlays = () => {
   return Play.find({}).populate("playsInstances")
+}
+
+const getOnePlay = async (playId, playInstanceId) => {
+  // const findOnePlay = await Play.findById(playId).populate("playsInstances")
+  // console.log(findOnePlay[0].playsInstances)
+    const p = await Play.findById(playId)
+    const pI = await PlayInstance.findById(playInstanceId)
+    console.log(p, pI)
+    return  {play: p, playInstance: pI}
+  
 }
 
 //home page
@@ -27,8 +38,14 @@ router.get('/signup', (req,res)=>{
 })
 
 // play page 
-router.get("/play", (req,res) => {
-  res.render("play", {title:"Plays", allplays: allPlays })
+router.get('/play/:PlayId/:playInstanceId', async (req,res) => {
+  const playId = req.params.PlayId
+  const playInstanceId = req.params.playInstanceId
+
+ let onePlay = await getOnePlay(playId, playInstanceId)
+ console.log("test")
+ console.log(onePlay)
+  res.render("play", { title:"Plays", play: onePlay })
 })
 
 //// play review page

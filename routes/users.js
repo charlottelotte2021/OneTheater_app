33aafaefe32bff95d6981c4f729505aba1175ceb
charlottelotte2021 <charlotteguen@gmail.com>
2022-04-06 15:30:46 +0200

    end of update profilepage

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ router.get("/login", (req, res) => {
     layout: "layouts/no-footer",
     title: "Log in",
     user: req.user,
-  })
+})
 })
 
 //Register handle
@@ -23,7 +23,7 @@ router.get("/signup", (req, res) => {
     layout: "layouts/no-footer",
     title: "Sign up",
     user: req.user,
-  })
+})
 })
 
 router.post("/signup", (req, res) => {
@@ -34,18 +34,18 @@ router.post("/signup", (req, res) => {
 
   if (!username || !email || !password || !password2) {
     errors.push({ msg: "Please fill in all fields" })
-  }
+}
   //check if match
   if (password !== password2) {
     errors.push({ msg: "The passwords don't match" })
-  }
+}
 
   //check if password is more than 6 characters
   if (password.length < 6) {
     errors.push({ msg: "Your password should be at least 6 characters long" })
-  }
+}
 
-  if (errors.length > 0) {
+if (errors.length > 0) {
     res.render("signup", {
       errors: errors,
       username: username,
@@ -55,15 +55,15 @@ router.post("/signup", (req, res) => {
       layout: "layouts/no-footer",
       title: "Sign up",
       user: req.user,
-    })
-  } else {
+  })
+} else {
     //validation passed
     User.findOne({ email }).exec((err, user) => {
       // console.log(user)
       if (user) {
         errors.push({
           msg: `An account with the email ${email} already exists`,
-        })
+      })
         res.render("signup", {
           errors: errors,
           username: username,
@@ -73,13 +73,13 @@ router.post("/signup", (req, res) => {
           layout: "layouts/no-footer",
           user: req.user,
           title: "Sign up",
-        })
-      } else {
+      })
+    } else {
         User.findOne({ username }).exec((err, user) => {
           if (user) {
             errors.push({
               msg: `An account with the name ${username} already exists, please choose another one.`,
-            })
+          })
             res.render("signup", {
               errors: errors,
               username: username,
@@ -89,35 +89,35 @@ router.post("/signup", (req, res) => {
               layout: "layouts/no-footer",
               user: req.user,
               title: "Sign up",
-            })
-          } else {
+          })
+        } else {
             const newUser = new User({
               username: username,
               email: email,
               password: password,
-            })
+          })
 
             bcrypt.genSalt(10, (err, salt) =>
               bcrypt.hash(newUser.password, salt, (err, hash) => {
                 if (err) throw err
                 //save pass to hash
-                newUser.password = hash
+            newUser.password = hash
                 //save user
                 newUser
-                  .save()
-                  .then((value) => {
+                .save()
+                .then((value) => {
                     console.log(value)
                     req.flash("success_msg", "You have now registered!")
                     res.redirect("/")
-                  })
-                  .catch((value) => console.log(value))
-              })
-            )
-          }
-        })
-      }
+                })
+                .catch((value) => console.log(value))
+            })
+              )
+        }
     })
-  }
+    }
+})
+}
 })
 
 router.post("/login", (req, res, next) => {
@@ -125,7 +125,7 @@ router.post("/login", (req, res, next) => {
     successRedirect: "/",
     failureRedirect: "/users/login",
     failureFlash: true,
-  })(req, res, next)
+})(req, res, next)
 })
 
 //profile page
@@ -144,86 +144,127 @@ router.get("/wishlist", (req,res) => {
 
 router.post('/profile', ensureAuthenticated, async (req, res) => {
 
-   const loggedUser = req.user
-    const {username,email, password, password2} = req.body;
-        let errors = [];
+ const loggedUser = req.user
+ const {username, email, password, password2} = req.body
+ let errors = []
 
-        const updatedUser = {
-              username: req.body.username,
-              email: req.body.email,
-              password: req.body.password
-            }
-//check if all fields are filled
-    if(!username || !email || !password || !password2) {
-        errors.push({msg : "Please fill in all fields"})
-    } 
-//check if passwords match 
-    if(password !== password2) {
-        errors.push({msg : "passwords dont match"});
+ const updatedUser = {}
+
+    if (req.body.username) {
+        updatedUser.username = req.body.username
+        }
+    if (req.body.email) {
+        updatedUser.username
+        }
+    if (req.body.password) {
+        updatedUser.password
     }
-//check if the password has at least 6 characters 
-    if(password.length < 6 ) {
-        errors.push({msg : 'password atleast 6 characters'})
-    } 
 
-     if(errors.length > 0 ) {
-        
-    res.render('profile', {
-        errors : errors,
-        username : username,
-        email : email,
-        password : password,
-        password2 : password2,
-        title: "Profile page",
-        user: req.user})
-    } else {
+    if (req.body.password2) {
+        updatedUser.password2                    
+     }
 
-//check if the e-mail already exists 
-    User.findOne({ email }).exec((err, user) => {
-      console.log(user) 
-      if (user) {
-        errors.push({
-          msg: `An account with the email ${email} already exists`,
+
+    //check if all fields are filled
+    // if(!username || !email || !password || !password2) {
+    //     errors.push({ msg : "Please fill in all fields" })
+    // } 
+    //check if passwords match 
+    if(password !== password2) {
+        errors.push({ msg : "passwords dont match" });
+    }
+    //check if the password has at least 6 characters 
+    // if(password.length < 6 ) {
+    //     errors.push({ msg : 'password atleast 6 characters' })
+    // } 
+
+    if(errors.length > 0 ) {
+
+        res.render('profile', {
+            errors : errors,
+            username : username,
+            email : email,
+            password : password,
+            password2 : password2,
+            title: "Profile page",
+            user: req.user
         })
+
     } else {
 
-//check if username already exists 
-    User.findOne({ username }).exec( async (err, user) => {
-        console.log(user)
-          if (user) {
-            errors.push({
-              msg: `An account with the name ${username} already exists, please choose another one.`,
-            })
-        }  else {
-            
-           await User.findByIdAndUpdate({ _id: req.user._id }, updatedUser)
-                 
-                     console.log(updatedUser)
-                const user = await User.findById(req.user._id)
+        //check if the e-mail already exists 
+        User.findOne({ email }).exec( async (err, user) => {
+            console.log(user) 
+            if (user) {
+               errors.push({
+                msg: `An account with the email ${email} already exists`,
 
-                bcrypt.genSalt(10, (err,salt) => 
-                bcrypt.hash(user.password,salt,
-                (err,hash) => {
-                    if(err) throw err;
+            })
+
+               res.render('profile', {
+                errors : errors,
+                username : username,
+                email : email,
+                password : password,
+                password2 : password2,
+                title: "Profile page",
+                user: req.user,
+
+            })
+
+           } else {
+
+            //check if username already exists 
+            User.findOne({ username }).exec( async (err, user) => {
+               if (user) {
+                  errors.push({
+                    msg: `An account with the name ${username} already exists, please choose another one.`,
+                })
+
+                  res.render('profile', {
+                    errors : errors,
+                    username : username,
+                    email : email,
+                    password : password,
+                    password2 : password2,
+                    title: "Profile page",
+                    user: req.user,
+
+                })
+
+              } else {
+
+                 await User.findByIdAndUpdate({ _id: req.user._id }, updatedUser)
+
+                 console.log(updatedUser)
+                 const user = await User.findById(req.user._id)
+
+                 bcrypt.genSalt(10, (err,salt) => 
+                    bcrypt.hash(user.password,salt,
+                        (err,hash) => {
+                            if(err) throw err;
                         //save pass to hash
                         user.password = hash;
                         //save user
                         user.save()
                         .then((value)=>{
-                        console.log("g reusiis a update")
-                        req.flash('success_msg','You have now updated!')
-                        res.redirect("/users/profile");
-                                })
+                            
+                            req.flash('success_msg','You have now updated!')
+                            res.redirect("/users/profile");
+                        })
                         .catch(value=> console.log(value));
+                    })
+                    )      
+             }   
+         })
+        }
+    })
+    }
+
+})
 
 
-                                  
-             }));
-            }          
-})}})}})
 
-
- 
 
 //wishlist page
 router.get("/wishlist", (req, res) => {

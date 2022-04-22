c9911ddbac0956349080e7716379924f515dc4f0
kriika2 <kriika2@gmail.com>
2022-04-22 12:39:45 +0200

    Fixed bug that Infinite scroll appeared where it shouldnt

--- public/scripts/frontend.js
+++ public/scripts/frontend.js
@@ -7,7 +7,7 @@ const softModalSubmit = document.querySelector(
   '.soft-modal--content input[type="button"]'
 )
 const cardsList = document.querySelector(".cards-container")
-let playsShowed = 5
+
 
 let sortButton = document.querySelector("#options-sort-input")
 
@@ -261,12 +261,11 @@ const createPlaysCard = (plays, data) => {
       divDirector.insertAdjacentHTML("beforeend", play.director)
       divProduction.insertAdjacentHTML(
         "beforeend",
-        `${
-          play.production
-            ? play.production.length > 100
-              ? play.production.substr(0, 97) + "..."
-              : play.production
-            : ""
+        `${play.production
+          ? play.production.length > 100
+            ? play.production.substr(0, 97) + "..."
+            : play.production
+          : ""
         }`
       )
       let divPicReviews = document.createElement("div")
@@ -351,73 +350,4 @@ const createPlaysCard = (plays, data) => {
   }, 1000)
 }
 
-// Infinite Scroll: loader
-const getFiveMorePlays = (nbr) => {
-  fetch("/getfiveplays", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ limit: nbr, location: window.location }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log(data)
-      document.querySelector("body").insertAdjacentHTML(
-        "beforeend",
-        `<div class="loader-cont"><svg width="120" height="30" viewBox="0 0 120 30" xmlns="http://www.w3.org/2000/svg" fill="#fff">
-    <circle cx="15" cy="15" r="15">
-        <animate attributeName="r" from="15" to="15"
-                 begin="0s" dur="0.8s"
-                 values="15;9;15" calcMode="linear"
-                 repeatCount="indefinite" />
-        <animate attributeName="fill-opacity" from="1" to="1"
-                 begin="0s" dur="0.8s"
-                 values="1;.5;1" calcMode="linear"
-                 repeatCount="indefinite" />
-    </circle>
-    <circle cx="60" cy="15" r="9" fill-opacity="0.3">
-        <animate attributeName="r" from="9" to="9"
-                 begin="0s" dur="0.8s"
-                 values="9;15;9" calcMode="linear"
-                 repeatCount="indefinite" />
-        <animate attributeName="fill-opacity" from="0.5" to="0.5"
-                 begin="0s" dur="0.8s"
-                 values=".5;1;.5" calcMode="linear"
-                 repeatCount="indefinite" />
-    </circle>
-    <circle cx="105" cy="15" r="15">
-        <animate attributeName="r" from="15" to="15"
-                 begin="0s" dur="0.8s"
-                 values="15;9;15" calcMode="linear"
-                 repeatCount="indefinite" />
-        <animate attributeName="fill-opacity" from="1" to="1"
-                 begin="0s" dur="0.8s"
-                 values="1;.5;1" calcMode="linear"
-                 repeatCount="indefinite" />
-    </circle>
-</svg> </div>`
-      )
-      createPlaysCard(data.plays, data)
-      playsShowed = playsShowed + 5
-      window.addEventListener("scroll", addEventListenerToWindow)
-
-      // return data
-      // console.log('Success:', data);
-    })
-    .catch((error) => {
-      console.error("Error:", error)
-    })
-}
-
-// Infinite scroll: if the user's at the bottom of the page, load 5 more plays
-const addEventListenerToWindow = () => {
-  const { scrollTop, scrollHeight, clientHeight } = document.documentElement
-
-  if (clientHeight + scrollTop >= scrollHeight - 5) {
-    getFiveMorePlays(playsShowed)
-    window.removeEventListener("scroll", addEventListenerToWindow)
-  }
-}
-
-window.addEventListener("scroll", addEventListenerToWindow)
+export default createPlaysCard;
